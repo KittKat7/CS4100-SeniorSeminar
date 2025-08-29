46f39712510574bfcb5ff0d8d0b6ddbd981cba0c
KittKat <122490347+KittKat7@users.noreply.github.com>
2025-08-29 11:40:47 -0400

    Update to inserting date for posts

--- loadBlogPosts.js
+++ loadBlogPosts.js
@@ -1,9 +1,9 @@
 
 
-async function getBlogPostList(user, repo, path) {
-	const folderURL = "./contentindex.txt";
+async function getBlogPostList() {
+	const indexURL = "./contentindex.txt";
 
-	const data = await (await getRemoteContent(folderURL)).text();
+	const data = await (await getRemoteContent(indexURL)).text();
 	const contentList = data.split("\n");
 	var list = [];
 	for (var i = 0; i < contentList.length; i++) {
@@ -21,7 +21,7 @@ async function getRemoteContent(url) {
 }
 
 async function addBlogPosts(section, user, repo, path) {
-	var postURLs = await getBlogPostList(user, repo, path);
+	var postURLs = await getBlogPostList();
 
 	var section = document.getElementById(section);
 
@@ -33,8 +33,14 @@ async function addBlogPosts(section, user, repo, path) {
 
 		const postData = await (await getRemoteContent("./posts/" + postURLs[i])).text();
 
+		var postDate = "";
+		postDate += postURLs[i].substring(0, 4) + "-";
+		postDate += postURLs[i].substring(4, 6) + "-";
+		postDate += postURLs[i].substring(6, 8) + " ";
+		postDate += postURLs[i].substring(9, 13);
+
 		var converter = new showdown.Converter(),
-			text = postData,
+			text = postData.replace("{{ DATE }}", postDate),
 			html = converter.makeHtml(text);
 
 		post.innerHTML = html;
